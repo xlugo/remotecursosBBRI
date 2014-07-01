7ab7dd3279be7b85fba4a9975b73a8d0d6d0d9ba
root <root@localhost.com>
2014-07-01 16:07:19 -0500

    Se corrige el paquete

---
+++ cursosBBRI/test/unit/cursosBBRI/AlumnoControllerTests.groovy
@@ -0,0 +1,19 @@
+package cursosBBRI
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+import cursosBBRI.AlumnoController;
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(AlumnoController)
+class AlumnoControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
